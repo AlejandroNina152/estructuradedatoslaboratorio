8a430de994b61b4804f435a381ab034e4a7568e2
AlejandroNina152 <88396527+AlejandroNina152@users.noreply.github.com>
2022-02-16 20:13:23 -0400

    Add files via upload
    grupo 8

---
+++ grupo 8.py
@@ -0,0 +1,15 @@
+def maximo(a,b);
+    if xy;
+       return x
+    else:
+        return y
+    def minimo(a,b):
+        if x<y:
+            return x
+        else:
+            return y
+
+#programa principal
+        x=int(input("un numero: "))
+        y=int(input("otro numero: "))
+        print(maximo(x-3,minimo(x+2,y-5)))
